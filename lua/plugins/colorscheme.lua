0225a06f1f77741bf41f9ea7146e32c20a37393f
huiming.qiu <huiming.qiu@shopee.com>
2023-07-20 10:08:32 +0800

    add colorschema

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,11 +2,20 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
+  { "rebelot/kanagawa.nvim" },
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon"
+    },
+  },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
 }
