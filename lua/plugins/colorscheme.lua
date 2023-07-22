e8fe11446e6156cdb2c6b5c953cc06205525d171
qiuhuiming <1985849912zz@gmail.com>
2023-07-22 19:28:24 +0800

    use tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,11 +2,18 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
+  {
+    "folke/tokyonight.nvim",
+    opt = {
+      style = "storm",
+    },
+  },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
 }
