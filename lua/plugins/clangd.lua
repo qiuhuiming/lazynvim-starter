5a1e2930f2466feb696f1af002a58beaddba888a
huiming.qiu <huiming.qiu@shopee.com>
2024-07-16 18:19:53 +0800

    disable inlay_hint

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -1,11 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    inlay_hints = { enabled = false },
     servers = {
       clangd = {
         capabilities = {
-          offsetEncoding = 'utf-8'
-        }
+          offsetEncoding = "utf-8",
+        },
       },
     },
   },
