c79c582a68e53f49b5da71f058aacbb2f750ec40
qiuhuiming <1985849912zz@gmail.com>
2024-08-01 22:34:47 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,6 +10,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.formatting.black" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
